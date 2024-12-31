bfc9680813cf2f39e21514a345b61eba7c5498aa
dmschauer <dmschauer@fake.mail>
2024-12-31 05:12:52 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# wap-pattern-pyspark-aws-glue
+# Write-Audit-Publish (WAP) Pattern implemented in Pyspark (AWS Glue ETL)
+
+This repository shows how to implement the Write-Audit-Publish (WAP) pattern using:
+- Apache Spark
+- Apache Iceberg
+
+It's aimed at Data Engineers who want to get started quickly
+
+> [!WARNING]
+> This repository is for demonstratin purposes only:
+> - It's barebones by design, to not distract from the WAP pattern.
+> - I've included rather long, explainatory comments about things to do differently in a real life scenario.
+> - It's meant to get you started quickly and to explain things, not to be production code.
+
+
+> [!TIP] 
+> Check out my other repository, in case you're interested in [a professional setup for Glue and Pyspark development](https://github.com/dmschauer/glue-pyspark-local).
+
+# How to use this?
+
+In your AWS account:
+- Create an S3 bucket
+- Create IAM role with matching permissions for Glue and S3 access
+- Create a Glue 5.0 ETL job
+  - Copy paste the code into the job
+  - Run it
+- (optional) Check results in Athena
+
+> [!IMPORTANT]
+> Using Glue 5.0 is important. The Iceberg branching feature was introduced in Iceberg version 1.2.0. Out of the box Glue 4.0 only supports Iceberg version 1.0.0 though, but Glue 5.0 supports Iceberg 1.6.1. [Details in the AWS Glue Documentation](https://docs.aws.amazon.com/glue/latest/dg/aws-glue-programming-etl-format-iceberg.html).
+> 
+> You can also make Glue 4.0 work but it requires significant setup effort by bringing your own Iceberg JAR files, instructions can be found [in the AWS Iceberg Precriptive Guidance](https://docs.aws.amazon.com/prescriptive-guidance/latest/apache-iceberg-on-aws/iceberg-glue.html). The Iceberg JARs can be found [on the official Iceberg website](https://iceberg.apache.org/releases/#downloads).
+
+> [!TIP] 
+> Most of the code can also be used without AWS access. It's mostly pure Pyspark code.
+
+
+# Appendix A: What is Write-Audit-Publish?
+
+Write-Audit-Publish (WAP) is a popular pattern in Data Engineering. Its purpose is to avoid publishing bad data.
+
+The idea is to run data quality checks on data _after computation, but before it is published_ to consumers.
+
+The WAP pattern involves three key stages:
+1. Write: Data is written to an audit branch
+2. Audit: Quality checks are performed on the audit branch
+3. Publish: Validated data is merged into the main branch (or not, in case the checks failed)
+
+![WAP pattern in Iceberg](img/wap-iceberg-branch.png)
+Image source: https://aws.amazon.com/blogs/big-data/build-write-audit-publish-pattern-with-apache-iceberg-branching-and-aws-glue-data-quality/
+
+A common mistake is to do data quality checks only retroactively, after bad data was already made available to consumers.
+
+In general, this should be avoided because the bad data could already be accessed by consumers (and then propagated to their own consumers etc.) while you're still fixing the mistakes.
+
+It's much cheaper to avoid this situation then to fix things after the damage was already done
+
+An even worse mistake would be to not run data quality checks at all, leaving your consumers to discover errors by chance, maybe only after malinfored decisions were made.
+
+What you gain by using WAP:
+- Saving _lots_ of time and money by avoiding mistakes that are costly to fix
+- Running ETL pipelines with confidence
+- Establish trust in your work, and avoiding to erode it
+
+Costs of using WAP:
+- Additional complexity (only a little though)
+- General engineering cost of implementing data quality checks
+- (Cloud) cost for additional processing power required
+- Pipeline runs longer, making it harder to meet SLAs
+
+In general the pros far outweigh the downsides. The more important the data the clearer it becomes.
+
+# Appendix B: What if they just want the data?
+
+Sometimes consumers want you to publish data that isn't quality checked. 
+
+Unchecked data runs the risk of being faulty. As a Data Engineer you should __strongly__ discourage this practice. 
+
+If your word of caution doesn't suffice, make it an explicit, written agreement with each consumer. In the agreement, highlight that consumers were warned about:
+- the risk of causing malinformed business decisions
+- the risk of propagating faulty data, which might become an organization-wide effort to fix
+
+You shouldn't be the one to blame in case they specically requested this risky practice you advised them not to do.
+
+Keep it simple though. A simple email you send them and they reply by acknowleding the risks could be enough for that. 
+
+# References used
+
+- https://aws.amazon.com/blogs/big-data/build-write-audit-publish-pattern-with-apache-iceberg-branching-and-aws-glue-data-quality/ 
+- https://docs.aws.amazon.com/glue/latest/dg/aws-glue-programming-etl-format-iceberg.html
+- https://docs.aws.amazon.com/prescriptive-guidance/latest/apache-iceberg-on-aws/iceberg-glue.html
+- https://iceberg.apache.org/releases/
+- https://iceberg.apache.org/docs/latest/branching/
+- https://www.guptaakashdeep.com/wap-via-apache-iceberg-on-aws/
+- 
---
+++ etl.py
@@ -0,0 +1,188 @@
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from pyspark.sql import SparkSession, DataFrame
+import uuid
+
+def create_iceberg_glue_session(warehouse_bucket: str, catalog_name: str) -> tuple[GlueContext, SparkSession]:
+    conf_list = [
+        ("spark.sql.session.timeZone", "UTC"),
+        ("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions"),
+        (f"spark.sql.catalog.{catalog_name}", "org.apache.iceberg.spark.SparkCatalog"),
+        (f"spark.sql.catalog.{catalog_name}.warehouse", f"s3://{warehouse_bucket}/{catalog_name}/"),
+        (f"spark.sql.catalog.{catalog_name}.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog"),
+        (f"spark.sql.catalog.{catalog_name}.io-impl", "org.apache.iceberg.aws.s3.S3FileIO"),
+        (f"spark.sql.catalog.{catalog_name}.glue.skip-name-validation", "true"),
+        ("spark.sql.iceberg.handle-timestamp-without-timezone", "true"),
+        ("spark.sql.sources.partitionOverwriteMode", "dynamic"),
+    ]
+
+    sc = SparkContext.getOrCreate()
+    for key, value in conf_list:
+        sc._conf.set(key, value)
+
+    glue_context = GlueContext(sc)
+    spark_session = SparkSession(sc)
+    return glue_context, spark_session
+
+def setup_glue_iceberg_table(spark: SparkSession, catalog_name: str, database_name: str, table_name: str, s3_bucket: str) -> None:
+
+    full_table_name = f"{catalog_name}.{database_name}.{table_name}"
+
+    # Create Glue database
+    spark.sql(f"CREATE DATABASE IF NOT EXISTS {full_table_name}")
+
+    # Create blank Iceberg table
+    spark.sql(f"DROP TABLE IF EXISTS {full_table_name}")
+    spark.sql(f"""CREATE TABLE {full_table_name} (
+                id INT, 
+                name STRING, 
+                age INT) 
+                LOCATION 's3://{s3_bucket}/{database_name}/{table_name}'
+                TBLPROPERTIES ('table_type'='ICEBERG')""")
+    
+    # Initialize 'main' branch, otherwise the table would have no branch whatsoever,
+    # you need a base branch to branch off of for WAP though.
+    # When not explicitly creating a branch, Iceberg will create a default one which is
+    # also called 'main' when you insert data for the first time.
+    spark.sql(f"ALTER TABLE {full_table_name} CREATE BRANCH IF NOT EXISTS main")
+
+def read_data(spark: SparkSession) -> DataFrame:
+    # sample data
+    return spark.createDataFrame(data=[
+        (1, "Alice", 28),
+        (2, "Bob", 34),
+        (3, "Charlie", 23)
+    ], schema=["id", "name", "age"])
+
+def transform(df: DataFrame) -> DataFrame:
+    return df.filter(df.age > 25)
+
+def generate_branch_name(prefix: str = "branch") -> str:
+    return f"{prefix}_{uuid.uuid4().hex[:6]}"
+
+def wap_append(spark: SparkSession, catalog_name: str, database_name: str, table_name: str, data_df: DataFrame) -> None:
+
+    full_table_name = f"{catalog_name}.{database_name}.{table_name}"
+
+    try:
+        # WAP: Write
+        # Note: 
+        #  This setting is only enabled temporarily for the WAP pattern.
+        #  It could be enabled permanently as far as Spark and Iceberg are concerned.
+        #  When it is set, you can't use the Athena query SHOW CREATE TABLE will throw an error. 
+        #  An Athena SELECT query on the Iceberg table would still work even with this setting set though.
+        #  But anyways, we clean it up in the finally block to enable the SHOW CREATE TABLE query again.
+        spark.sql(f"ALTER TABLE {full_table_name} SET TBLPROPERTIES ('write.wap.enabled'='true')")
+
+        # List existing Iceberg table branches
+        # This is purely informative, it could be removed or logged as INFO in a real world scenario
+        spark.sql(f"SELECT * FROM {full_table_name}.refs").show()
+
+        # Write new data into a temporary branch        
+        audit_branch_name = generate_branch_name(prefix="audit_branch")
+        spark.sql(f"ALTER TABLE {table_name} DROP BRANCH IF EXISTS {audit_branch_name}")
+        spark.sql(f"ALTER TABLE {table_name} CREATE BRANCH {audit_branch_name}")
+        (data_df.write
+            .format("iceberg")
+            .mode("append")
+            .option("branch", audit_branch_name)
+            .save(path=table_name))
+
+        # WAP: Audit
+        # Audit temporary branch
+        # Note:
+        #  This is for demonstration, in a real world scenario you would want to do a more complex audit.
+        #  - You could for example refactor this function and inject a test suite to run on the branch_df.
+        #  - You could also differentiate between the severity of failures, i.e. "warning" or "failing" checks.
+        # Note:
+        #  The Audit is very likely the most interesting part from a business value and analytics perspective.
+        #  Getting the business rules right is where you should focus your attention during development.
+        branch_df = spark.read \
+            .format("iceberg") \
+            .option("branch", audit_branch_name) \
+            .load(path=table_name)
+        audit_case_1 = branch_df.count() == data_df.count()
+        audit_case_2 = branch_df.count() > 0
+        audit_passed = audit_case_1 and audit_case_2
+
+        if audit_passed:
+            # WAP: Publish
+            # On the happy path all checks passed
+            # Publish changes from temporary branch to main branch
+            print("Audit passed. Publishing changes.")
+            spark.sql(f"CALL {catalog_name}.system.fast_forward('{table_name}', 'main', '{audit_branch_name}')")
+        else:
+            # WAP: Don't publish
+            # Because when a check failed we know there's an issue with the data
+            # Note:
+            #  In a real world scenario you would want to do a more complex Data Quality Check failure handling,
+            #  i.e. construct an audit report, send an email to the team, to the consumers, log the error, etc.
+            print("Audit failed. Reverting changes.")
+            spark.sql(f"ALTER TABLE {table_name} DROP BRANCH {audit_branch_name}")
+    except Exception as e:
+        raise
+        # Note:
+        #  In a real world scenario you would implement an actual error handling for specific error cases,
+        #  e.g. retry with exponential backoff, log the error, send an email to the team, etc.
+    finally:
+        # Cleanup
+        # Unset from table properties so that creating the table DDL statement in Athena will work again
+        spark.sql(f"ALTER TABLE {table_name} UNSET TBLPROPERTIES ('write.wap.enabled')")
+        
+    # The Audit is done. Thus the audit branch has served its purpose and can be deleted.
+    # Note:
+    #  This is explicitly NOT part of the finally block, because you might want to analyze
+    #  the data in the audit branch in case of data quality check failures and only delete it afterwards.
+    #  This is especially true in case computing the results is expensive.
+    #  You could also argue against this decision though, for example in case you need
+    #  to avoid manual interventions in prod altogether, or in case you know you won't analyze results anyway.
+    #  Being able to look at the faulty results is generally something you want though.
+    spark.sql(f"ALTER TABLE {table_name} DROP BRANCH IF EXISTS {audit_branch_name}")
+
+
+def main() -> None:
+    # Configuration
+    # Note: This is for demonstration only, in a real world scenario you would
+    # make these parameters of the Glue job and set them there, possibly by using Infrastructure as Code.
+    s3_bucket = "dominikschauer-iceberg-warehouse"
+    catalog_name = "glue_catalog"
+    database_name = "athena_iceberg_test2"
+    table_name = "my_iceberg_table2"
+    full_table_name = f"{catalog_name}.{database_name}.{table_name}"
+
+    # Create Spark session
+    glue_context, spark = create_iceberg_glue_session(warehouse_bucket=s3_bucket, 
+                                                      catalog_name=catalog_name)
+
+    # Infrastructure setup, read comments in function
+    # Note: 
+    #  In a real world scenario you would setup the infrastructure beforehand, 
+    #  likely by using Infrastructure as Code.
+    setup_glue_iceberg_table(spark=spark, 
+                             catalog_name=catalog_name, 
+                             database_name=database_name, 
+                             table_name=table_name, 
+                             s3_bucket=s3_bucket)
+
+    # ETL: Read data
+    # Note:
+    #  Here we only read sample data.
+    #  In a real world scenario you would read from an actual data source, possibly by using the glue_context.
+    df = read_data(spark=spark)
+
+    # ETL: Transform data
+    # Note: 
+    #  Simple transformation for demonstration, 
+    #  in a real world scenario you would likely transform the data in a more complex way.
+    transformed_df = transform(df=df)
+
+    # ETL: Write data
+    wap_append(spark=spark,
+              catalog_name=catalog_name, 
+              database_name=database_name,
+              table_name=full_table_name, 
+              data_df=transformed_df)
+
+
+if __name__ == "__main__":
+    main()
